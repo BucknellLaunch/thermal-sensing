0ab81af16cdf0b7f8501fb256c544321a79218cb
Jonathan Como <jonathan.como@gmail.com>
2014-01-21 19:46:28 -0500

    functionality to record comfort

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,29 @@ function index(req, res) {
 
 function recordcomfort(db) {
 	return function(req, res) {
-		// TODO
+		
+		// Get form values. We need the comfort level and location
+		var level = parseInt(req.body.level);
+		var location = req.body.location;
+		var now = new Date();
+
+		// Set the collection
+		var collection = db.get('comfort');
+
+		// Submit to the DB
+		collection.insert({
+			"level": level,
+			"location": location,
+			"timestamp": now.toJSON()
+		}, function (e, doc) {
+			if (e) {
+				res.send("There was an error adding to the DB");
+			}
+			else {
+				res.location('/');
+				res.redirect('/');
+			}
+		});
 	}
 }
 
