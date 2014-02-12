15bda7eb9c96045ce3ff50461e6f97f979f278de
Jonathan Como <jonathan.como@gmail.com>
2014-02-12 15:58:30 -0500

    Revert "favicon added"
    This reverts commit 53ae51d058790d83eb3e699655e6bce0b0ada946.

--- web.js
+++ web.js
@@ -18,7 +18,7 @@ var app = express();
 app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon(path.join(__dirname, 'public/img/favicon.ico')));
+app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
