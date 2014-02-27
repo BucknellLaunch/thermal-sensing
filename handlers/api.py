6a518f60c1cb5f68a0621b9bb8c06c57b2d38586
Jonathan Como <jonathan.como@gmail.com>
2014-02-27 15:42:10 -0500

    first api handler added for locations

--- handlers/api.py
+++ handlers/api.py
@@ -1,8 +1,26 @@
 from base import BaseHandler
 from models import Comfort, Location
+from google.appengine.api import memcache
+
+MC_LOCATIONS_KEY = 'LOCATIONS'
 
 class LocationsAPI(BaseHandler):
 	def get(self):
-		# check if its in the cache
-		locations = [str(location) for location in Location.all()]
-		self.render_json(locations)
+		locations = memcache.get(MC_LOCATIONS_KEY)
+
+		if not locations:
+			locations = list(Location.all())
+			memcache.set(MC_LOCATIONS_KEY, locations)
+
+		building = self.request.get('building')
+		floor = self.request.get('floor')
+
+		if building:
+			if floor:
+				print 'floor'
+				f = lambda loc: loc.building == building and loc.floor == floor
+			else:
+				f = lambda loc: loc.building == building
+			locations = filter(f, locations)
+
+		self.render_json([str(location) for location in locations])
